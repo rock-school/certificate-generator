5954506a74fcc29f37dc9f3a25cb85e5d798fa6d
OlgaR <katze.chan1@gmail.com>
2023-10-17 16:46:29 +0300

    no message

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,4 +1,25 @@
 const form = document.querySelector('.mainForm');
+
+function ValidateEmail(email) 
+{
+ if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/.test(email))
+  {
+    return true;
+  }
+
+    return false;
+}
+
 form.addEventListener('submit', () => {
-  window.location.href = 'congratulations.html'
+  const emailInput = document.querySelector('.email input');
+  const emailValue = emailInput.value;
+
+  const isEmailCorrect = ValidateEmail(emailValue);
+  if (isEmailCorrect) {
+      const formElement = document.forms.sertificate;
+      const fromData = new FormData(formElement);
+      window.location.href = 'congratulations.html'
+  } else {
+    alert("You have entered an invalid email address!")
+  }
 })
